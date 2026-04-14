10d53e53d9d21995756a0e1f31db5ea12e3c35ef
Omneya Zayed <72omnia@gmail.com>
2026-04-14 19:15:54 +0200

    completed the final pipeline

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,7 @@
 import time, os, argparse, tqdm, logging, emoji, re
 import pandas as pd
 import nltk
-import pkg_resources
+import importlib.resources
 from symspellpy import SymSpell
 from langdetect import detect, DetectorFactory
 from deep_translator import GoogleTranslator
@@ -13,7 +13,7 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%H:%M:%S",
 )
-logger = logging.getLogger(__name__)
+
 
 
 nltk.download('stopwords', quiet=True)
@@ -23,11 +23,14 @@ DetectorFactory.seed = 0
 
 class Preprocessing:
 
-    def __init__(self) -> None:
+    def __init__(self, args) -> None:
+        self.args = args
         self.sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-        self.dictionary_path = pkg_resources.resource_filename(
-        "symspellpy", "frequency_dictionary_en_82_765.txt"
-    )
+
+        with importlib.resources.path("symspellpy", "frequency_dictionary_en_82_765.txt") as path:
+            self.sym_spell.load_dictionary(str(path), term_index=0, count_index=1)
+    
+        self.logger = logging.getLogger(__name__)
 
     # Function to drop duplicates
     def clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -38,28 +41,30 @@ class Preprocessing:
         try:
             return detect(text)
         except:
-            logger.info("Unknown language found")
+            self.logger.info("Unknown language found")
             return 'unknown'
     
     # A function to translate the tweet or drop the record altogether
-    def handle_language(self, df: pd.DataFrame, mode='drop') -> pd.DataFrame:
+    # def handle_language(self, df: pd.DataFrame) -> pd.DataFrame:
 
-        df['language'] = df['text'].apply(self.detect_language)
-        df = df[df['language'] != 'unknown'].reset_index(drop=True)
-        df['text'].apply(self.translate)
+    #     df['language'] = df['text'].apply(self.detect_language)
+    #     df = df[df['language'] != 'unknown'].reset_index(drop=True)
+    #     df['text'].apply(self.translate)
 
-        return df.drop(columns=['language'])
+    #     return df.drop(columns=['language'])
 
     # A function to translate to English if the text was in a different language
     def translate(self, text: str) -> str:
 
-        if self.detect_language(text) == 'en':
+        lang = self.detect_language(text)
+
+        if lang == 'en':
             return text
         
         try:
-            return GoogleTranslator(source='auto', target='en')
+            return GoogleTranslator(source=lang, target='en')
         except:
-            logger.error("Failed to trnaslate to English")
+            self.logger.error("Failed to trnaslate to English")
             return text
         
     # Replaces emojis
@@ -86,5 +91,33 @@ class Preprocessing:
         blob = TextBlob(text)
         filtered = [word for word, tag in blob.tags if tag.startswith('NN') or tag.startswith('VB')]
         return " ".join([Word(w).lemmatize() for w in filtered])
+    
+    def process_text(self, text: str) -> str:
+        
+        if not isinstance(text, str) or text.strip() == "":
+            return ""
+        
+        if self.args.translate:
+            text = self.translate(text)
+
+        if self.args.no_urls:
+            text = self.remove_urls(text)
+        
+        if self.args.no_emoji:
+            text = self.remove_emoji(text)
+            
+        if self.args.do_spelling:
+            text = self.fix_spelling(text)
+            
+        if self.args.do_lemma:
+            text = self.lemmetize(text)
+        
+        if self.args.no_punctuation:
+            text = self.remove_punctuation(text)
+
+        if self.args.no_stopwords:
+            text = self.stop_words_removal(text)
+            
+        return text.strip()
     
     
